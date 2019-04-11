fd9cb61ee74af495194f8074a585eaf6bdb747cd
TomDom9 <32420396+TomDom9@users.noreply.github.com>
2019-04-10 20:40:24 -0400

    completed part 2 of the Django tutorial booyahkasha

--- ClimbingTracker/HelloApp/admin.py
+++ ClimbingTracker/HelloApp/admin.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)
+
+#we added this outside of the tutorial to be able to alter the choices for the questions
+admin.site.register(Choice)
