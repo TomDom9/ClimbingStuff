5325ca8415b4e3d0da9c3a9dc90bfb9051198a55
TomDom9 <32420396+TomDom9@users.noreply.github.com>
2019-04-10 21:30:54 -0400

    completed part 3 of the tutorial and updated detail.html/views to the path url because of version 2.2 for the tutorial. booyahkasha

--- ClimbingTracker/HelloApp/urls.py
+++ ClimbingTracker/HelloApp/urls.py
@@ -1,12 +1,20 @@
-from django.conf.urls import url
+#from django.conf.urls import url
+from django.urls import path
 from . import views
 app_name = 'HelloApp'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+# urlpatterns = [
+#     path('')
+#     url(r'^$', views.IndexView.as_view(), name='index'),
+#     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+#     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+#     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+#]
 
 
     #ex: /HelloApp/
